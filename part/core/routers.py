ea1a5bf3b46f0e78a46e5ad942e9624069ba8271
Tredikt <117392720+Tredikt@users.noreply.github.com>
2025-02-09 12:27:26 +0300

    add router to configuration

--- part/core/routers.py
+++ part/core/routers.py
@@ -1,9 +1,11 @@
 from core.handlers.my_id_handler import my_id_router
+from core.utils import configure_router
 
 from admin.routers import admin_routers
 from menu.routers import menu_routers
 
 routers = [
+    configure_router,
     my_id_router,
     *admin_routers,
     *menu_routers
---
+++ part/core/utils/__init__.py
@@ -0,0 +1,1 @@
+from core.utils.configure import configure_router
